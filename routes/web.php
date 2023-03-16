6475ea3a8cec7ec3a6e434e4a8f752c4f34fb908
Anwar-Barakat <brkatanwar0@gmail.com>
2023-03-16 11:54:38 +0300

    add admin login and forget password pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend.auth.login');
+});
+
+Route::get('/forget', function () {
+    return view('backend.auth.forget');
 });
