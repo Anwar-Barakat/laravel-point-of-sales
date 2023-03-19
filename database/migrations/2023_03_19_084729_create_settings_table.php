cddbfe77a718fc3c82cb3bf5ec6bb018fbde6408
Anwar-Barakat <brkatanwar0@gmail.com>
2023-03-19 11:55:41 +0300

    add setting table

---
+++ database/migrations/2023_03_19_084729_create_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('system_name');
+            $table->string('general_alert');
+            $table->string('address');
+            $table->string('mobile');
+            $table->integer('company_code');
+            $table->integer('added_by');
+            $table->integer('updated_by');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
