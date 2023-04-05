5173f8b1e5126829228b30e1b492e0d1f72d0e80
Anwar-Barakat <brkatanwar0@gmail.com>
2023-04-06 01:53:46 +0300

    finish account types

--- resources/views/admin/accounts/account-types/index.blade.php
+++ resources/views/admin/accounts/account-types/index.blade.php
@@ -37,37 +37,6 @@
                                 <td>
                                     <span class="badge bg-blue-lt">{{ $account_type->addedBy->name }}</span>
                                 </td>
-                                <td>
-                                    <span class="dropdown">
-                                        <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">{{ __('btns.actions') }}</button>
-                                        <div class="dropdown-menu dropdown-menu-end">
-                                            <a href="#" class="dropdown-item d-flex align-items-center gap-1" data-bs-toggle="modal" data-bs-target="#edit-account-type{{ $account_type->id }}">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon text-success" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-                                                    <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-                                                    <path d="M16 5l3 3" />
-                                                </svg>
-                                                <span>{{ __('btns.edit') }}</span>
-                                            </a>
-                                            <a href="#" class="dropdown-item d-flex align-items-center gap-1" data-bs-toggle="modal" data-bs-target="#modal-danger-{{ $account_type->id }}">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon m-0 text-danger" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M4 7l16 0" />
-                                                    <path d="M10 11l0 6" />
-                                                    <path d="M14 11l0 6" />
-                                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                                                </svg>
-                                                <span>{{ __('btns.delete') }}</span>
-                                            </a>
-                                        </div>
-                                    </span>
-
-                                    <x-modal-delete :id="$account_type->id" :action="route('admin.account-types.destroy', ['account_type' => $account_type])" />
-
-                                </td>
-                                <!-- edit account type modal -->
                             </tr>
                         @empty
                             <tr>
@@ -83,7 +52,5 @@
                 </div>
             </div>
         </div>
-
-        <!-- Add account type modal -->
     </div>
 </x-master-layout>
