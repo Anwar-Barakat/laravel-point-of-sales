dbfb05a18fa59cc6bef01f26cef3cfe1470e1cd8
Anwar-Barakat <brkatanwar0@gmail.com>
2023-05-19 22:44:32 +0300

    add service orders and service order details tables

---
+++ app/Http/Controllers/ServiceOrderDetailController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreServiceOrderDetailRequest;
+use App\Http\Requests\UpdateServiceOrderDetailRequest;
+use App\Models\ServiceOrderDetail;
+
+class ServiceOrderDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreServiceOrderDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ServiceOrderDetail $serviceOrderDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ServiceOrderDetail $serviceOrderDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateServiceOrderDetailRequest $request, ServiceOrderDetail $serviceOrderDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ServiceOrderDetail $serviceOrderDetail)
+    {
+        //
+    }
+}
