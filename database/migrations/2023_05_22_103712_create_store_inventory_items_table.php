d91c7cc7209a8d171e0df461d2e72f3d353fdbcd
Anwar-Barakat <brkatanwar0@gmail.com>
2023-05-22 13:51:58 +0300

    add store inventories and store inventory items tables

---
+++ database/migrations/2023_05_22_103712_create_store_inventory_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_inventory_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_inventory_id')->constrained()->cascadeOnUpdate();
+            $table->foreignId('item_id')->constrained()->cascadeOnUpdate();
+            $table->foreignId('unit_id')->comment('the main unit')->constrained()->cascadeOnUpdate();
+            $table->foreignId('item_batch_id')->constrained()->cascadeOnUpdate();
+            $table->integer('old_qty');
+            $table->integer('new_qty');
+            $table->integer('subtract')->comment('the difference between old and new qty');
+            $table->boolean('is_closed')->default(0);
+            $table->foreignId('added_by')->nullable()->constrained('admins')->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_inventory_items');
+    }
+};
