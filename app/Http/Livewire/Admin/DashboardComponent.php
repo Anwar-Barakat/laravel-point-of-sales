d6d4e432def56526b6cb76988cbc8292a5efb46c
Anwar-Barakat <brkatanwar0@gmail.com>
2023-06-03 14:40:17 +0300

    working on admin dashboard part(2) display sales statistics and what is paid and remain

--- app/Http/Livewire/Admin/DashboardComponent.php
+++ app/Http/Livewire/Admin/DashboardComponent.php
@@ -4,42 +4,78 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Order;
 use App\Models\Sale;
+use App\Models\WorkshopInvoice;
 use Livewire\Component;
 
 class DashboardComponent extends Component
 {
-    public $sales, $sale_filter;
+    public $order_filter;
+    public $sale_filter;
 
     public function render()
     {
         return view('livewire.admin.dashboard-component', [
-            'sales_result'   => $this->getSales(),
+            'orders_result'         => $this->getOrders(),
+            'sales_result'          => $this->getSales(),
         ]);
     }
 
-    public function getSales()
+    public function getOrders()
     {
-        $sale_filter = null;
-        if ($this->sale_filter == 2)
-            $sale_filter = now()->subMonth(1);
-        elseif ($this->sale_filter == 3)
-            $sale_filter = now()->subMonth(3);
-        else
-            $sale_filter = now()->subDays(7);
-
-        $total_sales = Order::where('company_id', get_auth_id())->where('type', 1)
-            ->where('created_at', '>=', $sale_filter)
+        $order_filter   = $this->getFilter($this->order_filter);
+        $total_orders   = Order::where('company_id', get_auth_id())->where('type', 1)
+            ->where('created_at', '>=', $order_filter)
             ->selectRaw('SUM(cost_after_discount) AS total_cost')
             ->first()
             ->toArray();
 
         $total_paid = Order::where('company_id', get_auth_id())->where('type', 1)
-            ->where('created_at', '>=', $sale_filter)
+            ->where('created_at', '>=', $order_filter)
             ->selectRaw('SUM(paid) AS paid_cost')
             ->first()
             ->toArray();
 
         $total_remaining = Order::where('company_id', get_auth_id())->where('type', 1)
+            ->where('created_at', '>=', $order_filter)
+            ->selectRaw('SUM(remains) AS remaining_cost')
+            ->first()
+            ->toArray();
+
+        $paid_percentage = 0;
+        if ($total_orders['total_cost'] > 0)
+            $paid_percentage = number_format($total_paid['paid_cost'] / $total_orders['total_cost'] * 100, 2);
+
+
+        $remaining_percentage = 0;
+        if ($total_orders['total_cost'] > 0)
+            $remaining_percentage = number_format($total_remaining['remaining_cost'] / $total_orders['total_cost'] * 100, 2);
+
+
+        return [
+            'total_orders'          => $total_orders['total_cost'],
+            'total_paid'            => $total_paid['paid_cost'],
+            'total_remaining'       => $total_remaining['remaining_cost'],
+            'paid_percentage'       => $paid_percentage,
+            'remaining_percentage'  => $remaining_percentage,
+        ];
+    }
+
+    public function getSales()
+    {
+        $sale_filter    = $this->getFilter($this->sale_filter);
+        $total_sales    = Sale::where('company_id', get_auth_id())->where('type', 1)
+            ->where('created_at', '>=', $sale_filter)
+            ->selectRaw('SUM(cost_after_discount) AS total_cost')
+            ->first()
+            ->toArray();
+
+        $total_paid     = Sale::where('company_id', get_auth_id())->where('type', 1)
+            ->where('created_at', '>=', $sale_filter)
+            ->selectRaw('SUM(paid) AS paid_cost')
+            ->first()
+            ->toArray();
+
+        $total_remaining = Sale::where('company_id', get_auth_id())->where('type', 1)
             ->where('created_at', '>=', $sale_filter)
             ->selectRaw('SUM(remains) AS remaining_cost')
             ->first()
@@ -62,5 +98,17 @@ class DashboardComponent extends Component
             'paid_percentage'       => $paid_percentage,
             'remaining_percentage'  => $remaining_percentage,
         ];
+    }
+
+    public function getFilter($filter)
+    {
+        $value = null;
+        if ($filter == 2)
+            $value = now()->subMonth(1);
+        elseif ($filter == 3)
+            $value = now()->subMonth(3);
+        else
+            $value = now()->subDays(7);
+        return $value;
     }
 }
--- resources/views/livewire/admin/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard-component.blade.php
@@ -6,7 +6,56 @@
                      <div class="subheader">{{ __('transaction.purchase_bills') }}</div>
                      <div class="ms-auto lh-1">
                          <div class="dropdown">
-                             <select class="form-select border-0" id="" wire:model='sales'>
+                             <select class="form-select border-0" id="" wire:model='order_filter'>
+                                 <option value="1">{{ __('dash.last_7_days') }}</option>
+                                 <option value="2">{{ __('dash.last_month') }}</option>
+                                 <option value="3">{{ __('dash.last_3_months') }}</option>
+                             </select>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="h1 mb-3">{{ $orders_result['total_orders'] ?? '0.00' }}</div>
+                 <div>
+                     <div class="d-flex mb-2">
+                         <div>{{ __('transaction.paid_amount') }}</div>
+                         <div class="ms-auto">
+                             <span class="text-green d-inline-flex align-items-center lh-1">
+                                 {{ $orders_result['paid_percentage'] }}%
+                             </span>
+                         </div>
+                     </div>
+                     <div class="progress progress-sm">
+                         <div class="progress-bar bg-primary" style="width: {{ $orders_result['paid_percentage'] }}%" role="progressbar" aria-valuenow="{{ $orders_result['total_paid'] }}" aria-valuemin="0" aria-valuemax="{{ $orders_result['total_orders'] }}" aria-label="75% Complete">
+                             <span class="visually-hidden">{{ $orders_result['total_paid'] }} Complete</span>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="mt-4">
+                     <div class="d-flex mb-2">
+                         <div>{{ __('transaction.remain_amount') }}</div>
+                         <div class="ms-auto">
+                             <span class="text-red d-inline-flex align-items-center lh-1">
+                                 {{ $orders_result['remaining_percentage'] }}%
+                             </span>
+                         </div>
+                     </div>
+                     <div class="progress progress-sm">
+                         <div class="progress-bar bg-primary" style="width: {{ $orders_result['remaining_percentage'] }}%" role="progressbar" aria-valuenow="{{ $orders_result['total_paid'] }}" aria-valuemin="0" aria-valuemax="{{ $orders_result['total_orders'] }}" aria-label="75% Complete">
+                             <span class="visually-hidden">{{ $orders_result['total_paid'] }} Complete</span>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <div class="col-sm-6 col-lg-3">
+         <div class="card">
+             <div class="card-body">
+                 <div class="d-flex align-items-center">
+                     <div class="subheader">{{ __('transaction.sales_invoices') }}</div>
+                     <div class="ms-auto lh-1">
+                         <div class="dropdown">
+                             <select class="form-select border-0" id="" wire:model='sale_filter'>
                                  <option value="1">{{ __('dash.last_7_days') }}</option>
                                  <option value="2">{{ __('dash.last_month') }}</option>
                                  <option value="3">{{ __('dash.last_3_months') }}</option>
@@ -45,107 +94,6 @@
                          </div>
                      </div>
                  </div>
-             </div>
-         </div>
-     </div>
-     <div class="col-sm-6 col-lg-3">
-         <div class="card">
-             <div class="card-body">
-                 <div class="d-flex align-items-center">
-                     <div class="subheader">Revenue</div>
-                     <div class="ms-auto lh-1">
-                         <div class="dropdown">
-                             <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
-                             <div class="dropdown-menu dropdown-menu-end">
-                                 <a class="dropdown-item active" href="#">Last 7 days</a>
-                                 <a class="dropdown-item" href="#">Last 30 days</a>
-                                 <a class="dropdown-item" href="#">Last 3 months</a>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="d-flex align-items-baseline">
-                     <div class="h1 mb-0 me-2">$4,300</div>
-                     <div class="me-auto">
-                         <span class="text-green d-inline-flex align-items-center lh-1">
-                             8%
-                             <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
-                             <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                 <path d="M3 17l6 -6l4 4l8 -8" />
-                                 <path d="M14 7l7 0l0 7" />
-                             </svg>
-                         </span>
-                     </div>
-                 </div>
-             </div>
-             <div id="chart-revenue-bg" class="chart-sm"></div>
-         </div>
-     </div>
-     <div class="col-sm-6 col-lg-3">
-         <div class="card">
-             <div class="card-body">
-                 <div class="d-flex align-items-center">
-                     <div class="subheader">New clients</div>
-                     <div class="ms-auto lh-1">
-                         <div class="dropdown">
-                             <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
-                             <div class="dropdown-menu dropdown-menu-end">
-                                 <a class="dropdown-item active" href="#">Last 7 days</a>
-                                 <a class="dropdown-item" href="#">Last 30 days</a>
-                                 <a class="dropdown-item" href="#">Last 3 months</a>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="d-flex align-items-baseline">
-                     <div class="h1 mb-3 me-2">6,782</div>
-                     <div class="me-auto">
-                         <span class="text-yellow d-inline-flex align-items-center lh-1">
-                             0%
-                             <!-- Download SVG icon from http://tabler-icons.io/i/minus -->
-                             <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                 <path d="M5 12l14 0" />
-                             </svg>
-                         </span>
-                     </div>
-                 </div>
-                 <div id="chart-new-clients" class="chart-sm"></div>
-             </div>
-         </div>
-     </div>
-     <div class="col-sm-6 col-lg-3">
-         <div class="card">
-             <div class="card-body">
-                 <div class="d-flex align-items-center">
-                     <div class="subheader">Active users</div>
-                     <div class="ms-auto lh-1">
-                         <div class="dropdown">
-                             <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
-                             <div class="dropdown-menu dropdown-menu-end">
-                                 <a class="dropdown-item active" href="#">Last 7 days</a>
-                                 <a class="dropdown-item" href="#">Last 30 days</a>
-                                 <a class="dropdown-item" href="#">Last 3 months</a>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="d-flex align-items-baseline">
-                     <div class="h1 mb-3 me-2">2,986</div>
-                     <div class="me-auto">
-                         <span class="text-green d-inline-flex align-items-center lh-1">
-                             4%
-                             <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
-                             <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                 <path d="M3 17l6 -6l4 4l8 -8" />
-                                 <path d="M14 7l7 0l0 7" />
-                             </svg>
-                         </span>
-                     </div>
-                 </div>
-                 <div id="chart-active-users" class="chart-sm"></div>
              </div>
          </div>
      </div>
@@ -195,7 +143,7 @@
                              </div>
                              <div class="col">
                                  <div class="font-weight-medium">
-                                     78 Orders
+                                     78 sales
                                  </div>
                                  <div class="text-muted">
                                      32 shipped
