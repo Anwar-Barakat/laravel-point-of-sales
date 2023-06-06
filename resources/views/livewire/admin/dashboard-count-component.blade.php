0285521b2f42b063f98c9c09dced65d7d998128d
Anwar-Barakat <brkatanwar0@gmail.com>
2023-06-06 18:49:19 +0300

    working on admin dashboards part(5) display all customers, vendors ,delegates, and workshops accounts details

--- resources/views/livewire/admin/dashboard-count-component.blade.php
+++ resources/views/livewire/admin/dashboard-count-component.blade.php
@@ -1,7 +1,7 @@
  <div class="row row-deck row-cards">
      <div class="col-12">
          <div class="row row-cards">
-             <div class="col-sm-6 col-lg-3">
+             <div class="col-sm-6 col-lg-4">
                  <div class="card card-sm">
                      <div class="card-body">
                          <div class="row align-items-center">
@@ -28,7 +28,7 @@
                      </div>
                  </div>
              </div>
-             <div class="col-sm-6 col-lg-3">
+             <div class="col-sm-6 col-lg-4">
                  <div class="card card-sm">
                      <div class="card-body">
                          <div class="row align-items-center">
@@ -55,7 +55,7 @@
                      </div>
                  </div>
              </div>
-             <div class="col-sm-6 col-lg-3">
+             <div class="col-sm-6 col-lg-4">
                  <div class="card card-sm">
                      <div class="card-body">
                          <div class="row align-items-center">
@@ -83,7 +83,7 @@
                      </div>
                  </div>
              </div>
-             <div class="col-sm-6 col-lg-3">
+             <div class="col-sm-6 col-lg-4">
                  <div class="card card-sm">
                      <div class="card-body">
                          <div class="row align-items-center">
@@ -112,7 +112,7 @@
                      </div>
                  </div>
              </div>
-             <div class="col-sm-6 col-lg-3">
+             <div class="col-sm-6 col-lg-4">
                  <div class="card card-sm">
                      <div class="card-body">
                          <div class="row align-items-center">
@@ -138,7 +138,7 @@
                      </div>
                  </div>
              </div>
-             <div class="col-sm-6 col-lg-3">
+             <div class="col-sm-6 col-lg-4">
                  <div class="card card-sm">
                      <div class="card-body">
                          <div class="row align-items-center">
@@ -167,7 +167,196 @@
              </div>
          </div>
      </div>
-   
+     <div class="col-12">
+         <div class="row row-cards">
+             <div class="col-sm-6 col-lg-3">
+                 <div class="card card-sm">
+                     <div class="card-body">
+                         <div class="row align-items-center">
+                             <div class="col-auto">
+                                 <span class="bg-bitbucket-lt text-white avatar">
+                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                         <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                         <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1"></path>
+                                         <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                         <path d="M17 10h2a2 2 0 0 1 2 2v1"></path>
+                                         <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                         <path d="M3 13v-1a2 2 0 0 1 2 -2h2"></path>
+                                     </svg>
+                                 </span>
+                             </div>
+                             <div class="col">
+                                 <div class="font-weight-medium">
+                                     {{ __('stock.customers') }} ({{ App\Models\Customer::count() ?? 0 }})
+                                 </div>
+                                 <div class="text-muted">
+                                     <div class="flex justify-between gap-1">
+                                         <span>
+                                             @php
+                                                 $customers_credit = App\Models\Customer::join('accounts', 'customers.id', '=', 'accounts.customer_id')
+                                                     ->where('accounts.current_balance', '<', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.credit') }}
+                                             {{ $customers_credit ?? 0 }}
+                                         </span>
+                                         <span>
+                                             @php
+                                                 $customers_debit = App\Models\Customer::join('accounts', 'customers.id', '=', 'accounts.customer_id')
+                                                     ->where('accounts.current_balance', '>', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.debit') }}
+                                             {{ $customers_debit ?? 0 }}
+                                         </span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-sm-6 col-lg-3">
+                 <div class="card card-sm">
+                     <div class="card-body">
+                         <div class="row align-items-center">
+                             <div class="col-auto">
+                                 <span class="bg-green-lt text-white avatar">
+                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                         <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                                         <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                                         <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                                         <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
+                                     </svg>
+                                 </span>
+                             </div>
+                             <div class="col">
+                                 <div class="font-weight-medium">
+                                     {{ __('stock.vendors') }} ({{ App\Models\Vendor::count() ?? 0 }})
+                                 </div>
+                                 <div class="text-muted">
+                                     <div class="flex justify-between gap-1">
+                                         <span>
+                                             @php
+                                                 $vendors_credit = App\Models\Vendor::join('accounts', 'vendors.id', '=', 'accounts.vendor_id')
+                                                     ->where('accounts.current_balance', '<', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.credit') }}
+                                             {{ $vendors_credit ?? 0 }}
+                                         </span>
+                                         <span>
+                                             @php
+                                                 $vendors_debit = App\Models\Vendor::join('accounts', 'vendors.id', '=', 'accounts.vendor_id')
+                                                     ->where('accounts.current_balance', '>', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.debit') }}
+                                             {{ $vendors_debit ?? 0 }}
+                                         </span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-sm-6 col-lg-3">
+                 <div class="card card-sm">
+                     <div class="card-body">
+                         <div class="row align-items-center">
+                             <div class="col-auto">
+                                 <span class="bg-muted-lt text-white avatar">
+                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-bolt" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                         <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                                         <path d="M6 21v-2a4 4 0 0 1 4 -4h4c.267 0 .529 .026 .781 .076"></path>
+                                         <path d="M19 16l-2 3h4l-2 3"></path>
+                                     </svg>
+                                 </span>
+                             </div>
+                             <div class="col">
+                                 <div class="font-weight-medium">
+                                     {{ __('stock.delegates') }} ({{ App\Models\Delegate::count() ?? 0 }})
+                                 </div>
+                                 <div class="text-muted">
+                                     <div class="flex justify-between gap-1">
+                                         <span>
+                                             @php
+                                                 $delegates_credit = App\Models\Delegate::join('accounts', 'delegates.id', '=', 'accounts.delegate_id')
+                                                     ->where('accounts.current_balance', '<', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.credit') }}
+                                             {{ $delegates_credit ?? 0 }}
+                                         </span>
+                                         <span>
+                                             @php
+                                                 $delegates_debit = App\Models\Delegate::join('accounts', 'delegates.id', '=', 'accounts.delegate_id')
+                                                     ->where('accounts.current_balance', '>', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.debit') }}
+                                             {{ $delegates_debit ?? 0 }}
+                                         </span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-sm-6 col-lg-3">
+                 <div class="card card-sm">
+                     <div class="card-body">
+                         <div class="row align-items-center">
+                             <div class="col-auto">
+                                 <span class="bg-facebook-lt text-white avatar">
+                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-analytics" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                         <path d="M3 4m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z"></path>
+                                         <path d="M7 20l10 0"></path>
+                                         <path d="M9 16l0 4"></path>
+                                         <path d="M15 16l0 4"></path>
+                                         <path d="M8 12l3 -3l2 2l3 -3"></path>
+                                     </svg>
+                                 </span>
+                             </div>
+                             <div class="col">
+                                 <div class="font-weight-medium">
+                                     {{ __('account.workshops') }} ({{ App\Models\Workshop::count() ?? 0 }})
+                                 </div>
+                                 <div class="text-muted">
+                                     <div class="flex justify-between gap-1">
+                                         <span>
+                                             @php
+                                                 $workshops_credit = App\Models\Workshop::join('accounts', 'workshops.id', '=', 'accounts.workshop_id')
+                                                     ->where('accounts.current_balance', '<', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.credit') }}
+                                             {{ $workshops_credit ?? 0 }}
+                                         </span>
+                                         <span>
+                                             @php
+                                                 $workshops_debit = App\Models\Workshop::join('accounts', 'workshops.id', '=', 'accounts.workshop_id')
+                                                     ->where('accounts.current_balance', '>', 0)
+                                                     ->sum('accounts.current_balance');
+                                             @endphp
+                                             {{ __('account.debit') }}
+                                             {{ $workshops_debit ?? 0 }}
+                                         </span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
      <div class="col-lg-6">
          <div class="card">
              <div class="card-body">
